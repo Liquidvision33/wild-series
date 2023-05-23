f86b0cd8bd2b0baf52a80222b1d8808e92b20e42
EdouardH <ehsurfdkr@hotmail.com>
2023-05-23 11:32:21 +0200

    create database,  2 tables by doctrine process

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
@@ -16,5 +17,11 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
 
         ]);
+    }
+
+    #[Route('/{id}', name: 'program_show', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/programShow.html.twig', ['id' => $id]);
     }
 }
